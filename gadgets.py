c37e55779909935fa40d0f0d16db67e686a1949f
Kendal <kendal.yaman.001@student.uni.lu>
2019-03-13 17:59:31 +0100

    az

--- gadgets.py
+++ gadgets.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
 
                             #print str(hexdata[i+2:])
                             flag = 1
-                            gadget = hexdata[i+2:i+4+(int(sys.argv[3]) * 2)]  # gadget = hexdata[0 : 30]. Ici on multiplie par 2 le length
+                            gadget = hexdata[i:i+4+(int(sys.argv[3]) * 2)]  # gadget = hexdata[0 : 30]. Ici on multiplie par 2 le length
                             gadget = convertXCS(gadget)
                             offset = 0
                             #print str(hexdata[i:i + 2])
@@ -102,7 +102,7 @@ if __name__ == '__main__':
                             print ("gadget:\n")
                             for (address, size, mnemonic, op_str) in md.disasm_lite(gadget, offset):
                                 if mnemonic in branInst:
-                                    flag == 0
+                                    flag = 0
                             if flag == 1:
                                 for (address, size, mnemonic, op_str) in md.disasm_lite(gadget, offset):
                                     print (" %s %s ") % (mnemonic, op_str)
