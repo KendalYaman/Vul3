7d6b32b88bd25aeb9dd247da00f26d64597c5b6a
Kendal <kendal.yaman.001@student.uni.lu>
2019-03-13 16:47:31 +0100

    az

--- gadgets.py
+++ gadgets.py
@@ -88,12 +88,12 @@ if __name__ == '__main__':
                     for i, _ in enumerate(hexdata):
                         if str(hexdata[i:i + 2]) in retHex:
                             #print str(hexdata[i+2:])
-
+                            gadget = hexdata[0: int(sys.argv[3]) * 2]  # gadget = hexdata[0 : 30]. Ici on multiplie par 2 le length
+                            gadget = convertXCS(gadget)
+                            offset = 0
                             #print str(hexdata[i:i + 2])
                             for (address, size, mnemonic, op_str) in md.disasm_lite(gadget, offset):
-                                gadget = hexdata[0: int(sys.argv[3]) * 2]  # gadget = hexdata[0 : 30]. Ici on multiplie par 2 le length
-                                gadget = convertXCS(gadget)
-                                offset = 0
+
                                 print ("gadget: %s %s \n") % (mnemonic, op_str)
 
                     """ gadget = hexdata[0 : int(sys.argv[3]) * 2] #gadget = hexdata[0 : 30]. Ici on multiplie par 2 le length
