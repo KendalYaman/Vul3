18f66959b347e39824993815bf20856f7fab6183
Kendal <kendal.yaman.001@student.uni.lu>
2019-03-14 12:50:04 +0100

    az

--- gadgets.py
+++ gadgets.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
                             #print str(hexdata[i+2:])
                             flag = 1
 
-                            gadget = hexdata[i  - (int(sys.argv[3])* 2 * 15 ) : i]  # gadget = hexdata[0 : 30]. Ici on multiplie par 2 le length
+                            gadget = hexdata[i  - (int(sys.argv[3])* 2 * 15 ) : i + 2]  # gadget = hexdata[0 : 30]. Ici on multiplie par 2 le length
                             print (gadget)
                             gadget = convertXCS(gadget)
 
