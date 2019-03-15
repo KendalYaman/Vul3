18c38042527b952b048f053f0effd47108e41043
Kendal <kendal.yaman.001@student.uni.lu>
2019-03-15 10:02:58 +0100

    a

--- gadgets.py
+++ gadgets.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
                                 instList.append([mnemonic, op_str])
                                 #print ("  %s \n")% (mnemonic)
 
-                            for ( mnemonic, op_str) in instList[-int(sys.argv[3])-1: -1]:
+                            for ( mnemonic, op_str) in instList[-int(sys.argv[3]): -1]:
                                 if mnemonic in branInst:
                                     #print ("mauvais  %s \n") % (mnemonic)
                                     flag = 0
